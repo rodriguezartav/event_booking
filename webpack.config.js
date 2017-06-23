c2cfceb28029708da2981b8b002b7f0cf006e94c
Roberto <roberto@rodriguezartavia.com>
2017-06-23 15:45:36 -0600

    build with API URL

--- webpack.config.js
+++ webpack.config.js
@@ -51,7 +51,8 @@ module.exports = {
     plugins: [
     new webpack.DefinePlugin({
       'process.env': {
-        NODE_ENV: JSON.stringify('production')
+        NODE_ENV: JSON.stringify('production'),
+        API_URL: JSON.stringify('API_URL')
       }
     }),
     new CleanWebpackPlugin(["dist/*.js","dist/*.css","dist/*.html","dist/*.map","dist/*.gz"], {verbose: true}),
